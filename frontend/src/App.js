068e1050011c4ecc9d126d34545a5bdca5033ad8
khanhhd <hoang.dang.khanh@framgia.com>
2018-09-13 22:50:49 +0700

    add sign in template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
-
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-
-        </p>
-        <button type="button" className="btn btn-primary">Test boostrap css</button>
-      </div>
+      <div className="container">
+       <div className="card card-container">
+           <p id="profile-name" className="profile-name-card"></p>
+           <form className="form-signin">
+               <span id="reauth-email" className="reauth-email"></span>
+               <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+               <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+               <div id="remember" className="checkbox">
+                   <label>
+                       <input type="checkbox" value="remember-me"/> Remember me
+                   </label>
+               </div>
+               <button className="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+           </form>
+           <a href="#" className="forgot-password">
+               Forgot the password?
+           </a>
+       </div>
+     </div>
     );
   }
 }
